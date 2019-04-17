232c21d1bde71a8a9709c790c8757c8a5d6f72ce
phildow <phil@phildow.net>
2019-04-17 09:52:23 -0700

    renamed bash script variable

--- download_libs.sh
+++ download_libs.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-TENSORIO_URL=https://storage.googleapis.com/tensorio-build/
-LIB_TENSORFLOW_URL=$TENSORIO_URL/tensorflow
-# LIB_PROTOBUF_URL=$TENSORIO_URL/libprotobuf
-# LIB_NSYNC_URL=$TENSORIO_URL/nsync
+TENSORIO_BUILD_URL=https://storage.googleapis.com/tensorio-build/
+LIB_TENSORFLOW_URL=$TENSORIO_BUILD_URL/tensorflow
+# LIB_PROTOBUF_URL=$TENSORIO_BUILD_URL/libprotobuf
+# LIB_NSYNC_URL=$TENSORIO_BUILD_URL/nsync
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 FRAMEWORK_DIR=tensorflow.framework
