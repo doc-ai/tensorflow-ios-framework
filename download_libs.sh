f144c884f5dd27e4ba601eea0854f79d499416e5
phildow <phil@phildow.net>
2020-06-01 12:34:40 -0700

    update download libs script

--- download_libs.sh
+++ download_libs.sh
@@ -1,15 +1,14 @@
 #! /bin/sh
 
-TENSORIO_BUILD_URL=https://storage.googleapis.com/tensorio-build/r1.15
+TENSORIO_BUILD_URL=https://storage.googleapis.com/tensorio-build/r2.0
 
 # ordered newest to oldest
 # library is stored on google with name tensorflow_tf.version_our.version
 
-TENSORFLOW_1_15_1=tensorflow_1.15_1     # adds _FusedMatMul op
-TENSORFLOW_1_15_0=tensorflow_1.15_0     # initial 1.15 build
+TENSORFLOW_2_0_0=tensorflow_2.0_0     # initial 2.0 build
 
 
-LIB_TENSORFLOW_URL=$TENSORIO_BUILD_URL/$TENSORFLOW_1_15_1
+LIB_TENSORFLOW_URL=$TENSORIO_BUILD_URL/$TENSORFLOW_2_0_0
 # LIB_PROTOBUF_URL=$TENSORIO_BUILD_URL/libprotobuf
 # LIB_NSYNC_URL=$TENSORIO_BUILD_URL/nsync
 
