24c8f4059eb2c82b0f4c3520e3301d208a2396fc
Philip Dow <phil@phildow.net>
2021-03-09 16:59:43 -0800

    bump to 2.0.9 noops summary ops and custom namespace for double_convert

--- download_libs.sh
+++ download_libs.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-LIB_URL=https://storage.googleapis.com/tensorio-build/ios/release/2.0/xcodebuild/12C33/tag/2.0.8/lib
+LIB_URL=https://storage.googleapis.com/tensorio-build/ios/release/2.0/xcodebuild/12C33/tag/2.0.9/lib
 
 LIB_TENSORFLOW_URL=$LIB_URL/libtensorflow-core.a    # -> tensorflow
 # LIB_PROTOBUF_URL=$LIB_URL/libprotobuf.a           # -> libprotobuf.a
